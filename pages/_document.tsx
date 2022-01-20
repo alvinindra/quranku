aaf082007bd3acdb7a9d8cfcead32ca19f94070b
Alvin Indra <alvinindrap@gmail.com>
2022-01-20 14:17:55 +0700

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,7 @@ class MyDocument extends Document {
             type='font/ttf'
             crossOrigin='anonymous'
           />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
           <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet' />
