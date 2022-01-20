ffd42f44a65ff83ca7e9d2a77cfe875419531079
Alvin Indra <alvinindrap@gmail.com>
2022-01-20 16:58:22 +0700

    fix: fixing go to last read

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,20 @@ import ListSurah from '@/components/ListSurah'
 import { getItem } from '@/utils/storage'
 import storageKey from '@/constant/storage-key'
 import Head from 'next/head'
-import Link from 'next/link'
+import Router from 'next/router'
 
 export default function Home({listFullSurah}: any) {
   const cacheLastRead = getItem(storageKey.LAST_READ, storageKey.VERSION) || {}
   const hasCache = Object.keys(cacheLastRead).length > 0
+
+  const gotoLastRead = () => {
+    if (hasCache) {
+      Router.push(`/surah/${cacheLastRead.numberSurah}#${cacheLastRead.verse}`)
+    } else {
+      return
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Head>
@@ -17,49 +26,44 @@ export default function Home({listFullSurah}: any) {
       <Header title='Quranku'/>
 
       <main className="px-[20px] w-full">
-        <Link
-            href={`/surah/${cacheLastRead.numberSurah}#${cacheLastRead.verse}`}
-            passHref
-          >
-          <div className='relative cursor-pointer'>
-            <div className='absolute z-10 left-5 top-5'>
-              <div className='flex mb-6'>
-                <Image 
-                  className='rounded-lg w-full'
-                  src='/images/icon/lastread.svg'
-                  width={20}
-                  height={20}
-                  alt=''
-                />
-                <div className='text-sm ml-2 text-white'>Terakhir Dibaca</div>
-              </div>
-              { hasCache ?
-                <>
-                  <div className='font-bold text-base mb-[3px] text-white'>
-                    {cacheLastRead.surah}
-                  </div>
-                  <div className='text-xs text-white'>Ayat {cacheLastRead.verse}</div>
-                </>
-                : <>
-                  <div className='font-bold text-base mb-[3px] text-white'>
-                    <div>Ayo baca </div>
-                    <div>Al-Quran</div>
-                  </div>
-                </>
-              }
+        <div className={`relative ${hasCache ? 'cursor-pointer' : ''}`} onClick={() => gotoLastRead()}>
+          <div className='absolute z-10 left-5 top-5'>
+            <div className='flex mb-6'>
+              <Image 
+                className='rounded-lg w-full'
+                src='/images/icon/lastread.svg'
+                width={20}
+                height={20}
+                alt=''
+              />
+              <div className='text-sm ml-2 text-white'>Terakhir Dibaca</div>
             </div>
-            <Image 
-              className='rounded-lg w-full'
-              src='/images/bg-lastread.svg'
-              width={320}
-              height={130}
-              layout='responsive'
-              objectFit='cover'
-              priority
-              alt=''
-            />
+            { hasCache ?
+              <>
+                <div className='font-bold text-base mb-[3px] text-white'>
+                  {cacheLastRead.surah}
+                </div>
+                <div className='text-xs text-white'>Ayat {cacheLastRead.verse}</div>
+              </>
+              : <>
+                <div className='font-bold text-base mb-[3px] text-white'>
+                  <div>Ayo baca </div>
+                  <div>Al-Quran</div>
+                </div>
+              </>
+            }
           </div>
-        </Link>
+          <Image 
+            className='rounded-lg w-full'
+            src='/images/bg-lastread.svg'
+            width={320}
+            height={130}
+            layout='responsive'
+            objectFit='cover'
+            priority
+            alt=''
+          />
+        </div>
         <ListSurah listFullSurah={listFullSurah} />
       </main>
     </div>
