3a8e5212d4e7ee3364f7c7d30fde5d5129ded7bf
Alvin Indra <alvinindrap@gmail.com>
2022-01-20 20:13:54 +0700

    feat: add twitter meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,10 +11,14 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Baca Al-Quran dengan ayat terjemahan Bahasa Indonesia" />
+        <meta name="description" content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia" />
         <meta name="keywords" content="Al-Quran, Quran, Quran Indonesia, Quranku" />
         <meta name="og:title" content="Quranku" />
         <title>Quranku</title>
+        <meta name="og:image" content="/images/cover-quranku.jpg" />
+        <meta name="twitter:title" content="Quranku"/>
+        <meta name="twitter:description" content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#29A19C" />
       </Head>
