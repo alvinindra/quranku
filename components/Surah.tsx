92f06e4c5cbcf6630a7153d2909176d8616cba94
Alvin Indra <alvin@dicoding.com>
2023-08-16 21:03:18 +0700

    revamp: upgrade next js to v13

--- components/Surah.tsx
+++ components/Surah.tsx
@@ -6,21 +6,18 @@ interface typeItemObject {
 }
 
 export default function Surah({item}: typeItemObject) {
-  return (
-    <>
-      <Link
-        href={'/surah/'+item.index}
-        passHref
-      >
-        <a className='relative flex mb-5 px-3 py-4 bg-white rounded-lg drop-shadow-custom'>
-          <div className='mr-5 my-auto rounded-full w-6 h-6 text-center border border-[#29A19C] 
-          text-xs text-[#29A19C] flex justify-center'><span className='my-auto'>{item.index}</span></div>
-          <div className='my-auto'>
-            <div className='text-[#29A19C] font-semibold'>{item.latin}</div>
-            <div className='text-[#8A8A8E] text-xs'>{item.translation}, {item.ayah_count} ayat</div>
-          </div>
-        </a>
-      </Link>
-    </>
-  );
+  return <>
+    <Link
+      href={'/surah/'+item.index}
+      passHref>
+        <div className='relative flex mb-5 px-3 py-4 bg-white rounded-lg drop-shadow-custom cursor-pointer'>
+        <div className='mr-5 my-auto rounded-full w-6 h-6 text-center border border-[#29A19C] 
+      text-xs text-[#29A19C] flex justify-center'><span className='my-auto'>{item.index}</span></div>
+      <div className='my-auto'>
+        <div className='text-[#29A19C] font-semibold'>{item.latin}</div>
+        <div className='text-[#8A8A8E] text-xs'>{item.translation}, {item.ayah_count} ayat</div>
+      </div>
+        </div>
+    </Link>
+  </>;
 }
--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    runtimeCaching
-  }
-})
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
