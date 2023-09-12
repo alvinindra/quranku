73eaa0483f587851667681f63a302809fd2529c8
Alvin Indra <alvinindrap@gmail.com>
2023-09-12 20:27:42 +0700

    feat: integrate dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,18 +12,32 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia" />
-        <meta name="keywords" content="Al-Quran, Quran, Quran Indonesia, Quranku" />
+        <meta
+          name="description"
+          content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia"
+        />
+        <meta
+          name="keywords"
+          content="Al-Quran, Quran, Quran Indonesia, Quranku"
+        />
         <meta name="og:title" content="Quranku" />
         <title>Quranku</title>
-        <meta name="og:image" content="https://alquran-ku.netlify.app/images/cover-quranku.jpg" />
-        <meta name="twitter:title" content="Quranku"/>
-        <meta name="twitter:description" content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia" />
+        <meta
+          name="og:image"
+          content="https://alquran-ku.netlify.app/images/cover-quranku.jpg"
+        />
+        <meta name="twitter:title" content="Quranku" />
+        <meta
+          name="twitter:description"
+          content="Aplikasi baca Al-Quran dengan ayat terjemahan Bahasa Indonesia"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#29A19C" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -7,7 +8,8 @@ module.exports = {
     extend: {
       dropShadow: {
         custom: '0px 2px 16px rgba(138, 138, 142, 0.25)',
-      }
+        dark: '0px 3px 8px 4px #2F2F2F',
+      },
     },
   },
   plugins: [],
